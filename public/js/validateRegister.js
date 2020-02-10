487f19c3fe51a2a25a23242c332884d1bbe53a83
javi-dh <javier@digitalhouse.com>
2020-02-10 12:50:17 -0300

    Validation re-build

--- public/js/validateRegister.js
+++ public/js/validateRegister.js
@@ -7,58 +7,54 @@ camposDelFormulario.pop();
 
 let camposConError = {};
 
+// Generic validate function
+function validateInput(message, input, typeOfValidator) {
+	// Capturamos el valor del campo
+	let valorDelCampo = input.value.trim();
+	// trim() es un método que elimina los espacios vacíos al principio y al final
+
+	let validation;
+	
+	switch (typeOfValidator) {
+		case 'isEmail':
+			validation = !validator[typeOfValidator](valorDelCampo);
+			break;
+		default:
+			validation = validator[typeOfValidator](valorDelCampo);
+			break;
+	}
+
+	// si no se pasa la validación
+	if (validation) {
+		// Si el campo tiene error, agregamos la clase de boostrap 'is-invalid'
+		input.classList.add('is-invalid');
+		// Insertamos un mensaje de error en el div 'invalid-feedback'
+		input.nextElementSibling.innerHTML = `El campo <b>${input.getAttribute('data-name')}</b> ${message}`;
+		// Al objeto literal de errores, la asignamos una prop con el nombre del campo y valor true
+		camposConError[input.name] = true;
+	} else {
+		// Cuando no hay error, eliminamos la clase por si la tuviera
+		input.classList.remove('is-invalid');
+		input.classList.add('is-valid');
+		// Eliminamos el mensaje de error en el div 'invalid-feedback'
+		input.nextElementSibling.innerHTML = '';
+		// Al objeto literal de errores, le eliminamos la prop del campo que tenía error
+		delete camposConError[input.name];
+	}
+}
+
 // Iteramos sobre el array de campos
 for (const unCampo of camposDelFormulario) {
-
 	// A cada campo le pasamos el evento blur
 	unCampo.addEventListener('blur', function () {
-		// Capturamos el valor del campo
-		let valorDelCampo = unCampo.value.trim();
-		// trim() es un método que elimina los espacios vacíos al principio y al final
-		
-		// Sí el valor del campo está vacío
-		if (validator.isEmpty(valorDelCampo))  {
-			// Si el campo tiene error, agregamos la clase de boostrap 'is-invalid'
-			unCampo.classList.add('is-invalid');
-			// Insertamos un mensaje de error en el div 'invalid-feedback'
-			unCampo.nextElementSibling.innerHTML = `El campo <b>${unCampo.getAttribute('data-name')}</b> es obligatorio`;
-			// Al objeto literal de errores, la asignamos una prop con el nombre del campo y valor true
-			camposConError[unCampo.name] = true;
-		} else {
-			// Cuando no hay error, eliminamos la clase por si la tuviera
-			unCampo.classList.remove('is-invalid');
-			unCampo.classList.add('is-valid');
-			// Eliminamos el mensaje de error en el div 'invalid-feedback'
-			unCampo.nextElementSibling.innerHTML = '';
-			// Al objeto literal de errores, le eliminamos la prop del campo que tenía error
-			delete camposConError[unCampo.name];
-		}
+		validateInput('es obligatorio', this, 'isEmpty');
 	});
 
 	// Si el nombre del campo es 'email'
-	if (unCampo.name === 'email') {
+	if (unCampo.name === 'email') {	
 		unCampo.addEventListener('blur', function () {
-			// Capturamos lo que se escribío en el campo email
-			let valorDelCampo = unCampo.value.trim();
-
-			// Si el valor del campo NO es un formato de correo electrónico
-			if (!validator.isEmail(valorDelCampo)) {
-				// Si el campo tiene error, agregamos la clase de boostrap 'is-invalid'
-				unCampo.classList.remove('is-valid');
-				unCampo.classList.add('is-invalid');
-				// Insertamos un mensaje de error en el div 'invalid-feedback'
-				unCampo.nextElementSibling.innerHTML = `El campo <b>${unCampo.getAttribute('data-name')}</b> debe contener un formato de correo electrónico`;
-				// Al objeto literal de errores, la asignamos una prop con el nombre del campo y valor true
-				camposConError[unCampo.name] = true;
-				console.log(camposConError);
-			} else {
-				// Cuando no hay error, eliminamos la clase por si la tuviera
-				unCampo.classList.remove('is-invalid');
-				unCampo.classList.add('is-valid');
-				// Eliminamos el mensaje de error en el div 'invalid-feedback'
-				unCampo.nextElementSibling.innerHTML = '';
-				// Al objeto literal de errores, le eliminamos la prop del campo que tenía error
-				delete camposConError[unCampo.name];
+			if (!validator.isEmpty(unCampo.value)) {
+				validateInput('debe contener un formato de correo electrónico', this, 'isEmail');
 			}
 		})
 	}
